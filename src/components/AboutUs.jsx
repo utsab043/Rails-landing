ca2779a3bb782a68f11a32669b1dcf5e953b703a
utsab043 <grgutsab66@gmail.com>
2023-07-07 13:52:58 +0545

    done

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -11,11 +11,11 @@ const AboutUs = () => {
     <div id="About">
       <h2>About Us</h2>
       <div className="flex container">
-     <div data-aos="flip-up">
-      <AboutCard logo={<BookOpen />} title="Our Story" descp=" Currently, we are Software Engineering Students and as a part of the curriculum it's mandatory to do a college project for everyone.Therefore we decided to build RAILS as our college project this year "/>
+     <div data-aos="flip-up" className="story">
+      <AboutCard logo={<BookOpen />} title="Our Story" descp=" Currently, we are Software Engineering Students and as a part of the curriculum it's mandatory to do a college project for everyone.Therefore we decided to build RAILS as our college project this year. "/>
      </div>
-     <div data-aos="flip-up">
-     <AboutCard logo={<Flag />} title="Our Goal" descp=" Our main goal is to make the work for people more collaborartive, easier and scalable and Rails is the answer to that"/>
+     <div data-aos="flip-up" className="story">
+     <AboutCard logo={<Flag />} title="Our Goal" descp=" Our main goal is to make the work for people more collaborartive, easier and scalable and Rails is the answer to that."/>
      </div>
      </div>
      </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,22 @@
 import { ArrowRight } from "react-feather"
 const Footer = () => {
   return (
-    <div id="Footer" className="flex">
+    <div id="Footer" >
+      <div className="flex footer">
+        <div>
+        <h3 className="center">Contact Us</h3>
+        <p className="center">Send us a message</p>
+        <div className="flex contact">
+        <input type="text" placeholder="Name" className="input" />
+        <input type="email" placeholder="Email" className="input" />
+        <textarea rows={"5"} cols={"23"} placeholder="Message" className="input"></textarea>
+        <button className="btn-start btn-send flex gap-1 center">Send <ArrowRight /></button>
+      </div>
+      </div>
+      <div>
+      <img src="logo.svg" alt=""  className="logo"/>
       <p>2023 &copy; All rights reserved.</p>
-      <div className="contact">
-      <h3>Get in touch</h3>
-      <input type="email" placeholder="Email" />
-      <input type="text" placeholder="Message" />
-      <button className="btn-start flex gap-1">Send <ArrowRight /></button>
+      </div>
       </div>
       </div>
   )
