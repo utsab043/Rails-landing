49f4bddb8c62f5cf28cb6d1f22c47b7359f7b706
utsab043 <grgutsab66@gmail.com>
2023-07-07 10:55:03 +0545

    start of the day

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import { ArrowRight } from "react-feather"
 const Footer = () => {
   return (
-    <div id="Footer">Footer</div>
+    <div id="Footer" className="flex">
+      <p>2023 &copy; All rights reserved.</p>
+      <div className="contact">
+      <h3>Get in touch</h3>
+      <input type="email" placeholder="Email" />
+      <input type="text" placeholder="Message" />
+      <button className="btn-start flex gap-1">Send <ArrowRight /></button>
+      </div>
+      </div>
   )
 }
-
 export default Footer
